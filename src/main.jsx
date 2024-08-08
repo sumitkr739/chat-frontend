f68a2b38415ed204d298768b034764021f28b385
Sumit Kumar Sinha <116585363+sumitkr739@users.noreply.github.com>
2024-08-08 09:41:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { UserProvider } from "./context/UserContext.jsx";
 import { ChatProvider } from "./context/ChatContext.jsx";
 
-export const server = "http://localhost:5000";
+export const server = "https://chatserver-3g72.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
